cb9f28db59d6e3e9f0c3b1c12ab96113376503f7
BIANCA ESTEFANÍA <alu0100814231@ull.edu.es>
2014-02-28 10:16:47 +0000

    Ejercicios de la practica 4

---
+++ ej_21.py
@@ -0,0 +1,12 @@
+a = float(raw_input('Valor de a: '))
+b = float(raw_input('Valor de b: '))
+
+if a != 0:
+   x = -b/a
+   print 'Solucion: ', x
+
+if a = 0:
+   x = -b/a
+   print 'La ecuación no tiene solución', x
+
+  
